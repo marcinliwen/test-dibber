a95e060691b54d500196524d016adaa477361ec5
m.liwen <m.liwen@perfectone.pl>
2023-03-09 01:21:08 +0100

    map

--- public/script.js
+++ public/script.js
@@ -26,24 +26,23 @@ window.onload = (event) => {
           slidesPerView: 3,
           pagination: {
             enabled: true,
+            el: ".blog-pagination",
+            clickable: true,
+            renderBullet: function (index, className) {
+              return (
+                '<span class="' +
+                className +
+                '">' +
+                (index < 10 ? "0" : "") +
+                (index + 1) +
+                ".</span>"
+              );
+            },
           },
           slidesPerGroup: 3,
         },
       },
-      pagination: {
-        el: ".blog-pagination",
-        clickable: true,
-        renderBullet: function (index, className) {
-          return (
-            '<span class="' +
-            className +
-            '">' +
-            (index < 10 ? "0" : "") +
-            (index + 1) +
-            ".</span>"
-          );
-        },
-      },
+
       navigation: {
         nextEl: ".blog-next",
         prevEl: ".blog-prev",
@@ -52,6 +51,9 @@ window.onload = (event) => {
     new Swiper(".review-swiper", {
       slidesPerView: 1,
       spaceBetween: 32,
+      pagination: {
+        enabled: false,
+      },
       breakpoints: {
         760: {
           slidesPerView: 2,
@@ -60,23 +62,22 @@ window.onload = (event) => {
           slidesPerView: 3,
           pagination: {
             enabled: true,
+            el: ".review-pagination",
+            clickable: true,
+            renderBullet: function (index, className) {
+              return (
+                '<span class="' +
+                className +
+                '">' +
+                (index < 10 ? "0" : "") +
+                (index + 1) +
+                ".</span>"
+              );
+            },
           },
         },
       },
-      pagination: {
-        el: ".review-pagination",
-        clickable: true,
-        renderBullet: function (index, className) {
-          return (
-            '<span class="' +
-            className +
-            '">' +
-            (index < 10 ? "0" : "") +
-            (index + 1) +
-            ".</span>"
-          );
-        },
-      },
+
       navigation: {
         nextEl: ".review-next",
         prevEl: ".review-prev",
@@ -142,32 +143,82 @@ window.onload = (event) => {
       dropdown.classList.remove("opened");
     }
   };
-  // Event Listeners
+  if (typeof dropdown !== undefined && dropdown) {
+    console.log("dropdown");
+    // Event Listeners
 
-  body.addEventListener("click", closeDropdownFromOutside);
+    body.addEventListener("click", closeDropdownFromOutside);
 
-  listOfOptions.forEach((option) => {
-    option.addEventListener("click", selectOption);
-  });
+    listOfOptions.forEach((option) => {
+      option.addEventListener("click", selectOption);
+    });
 
-  dropdown.addEventListener("click", toggleDropdown);
+    dropdown.addEventListener("click", toggleDropdown);
+  }
 
   const childAge = document.querySelector(".child-age");
   const plus = document.getElementById("plus");
   const minus = document.getElementById("minus");
 
-  plus.addEventListener("click", function () {
-    let input = document.getElementById("child-age");
-    let val = input.value;
-    if (val <= 9) {
-      input.value = Number(val) + 1;
-    }
+  if (typeof plus !== undefined && plus) {
+    console.log("plus");
+    plus.addEventListener("click", function () {
+      let input = document.getElementById("child-age");
+      let val = input.value;
+      if (val <= 9) {
+        input.value = Number(val) + 1;
+      }
+    });
+  }
+
+  if (typeof minus !== undefined && minus) {
+    console.log("minus");
+    minus.addEventListener("click", function () {
+      let input = document.getElementById("child-age");
+      let val = input.value;
+      if (val > 0) {
+        input.value = Number(val) - 1;
+      }
+    });
+  }
+
+  var map = L.map("map");
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    // Specify the maximum zoom of the map
+    maxZoom: 19,
+
+    // Set the attribution for OpenStreetMaps
+    attribution:
+      '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  // Set the view of the map
+  // with the latitude, longitude and the zoom value
+  map.setView([48.8584, 2.2945], 16);
+
+  var customIcon = L.icon({
+    iconUrl: "/assets/marker.svg",
+    iconSize: [38, 95], // size of the icon
+    //shadowSize: [50, 64], // size of the shadow
+    //iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    //shadowAnchor: [4, 62], // the same for the shadow
+    //popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
   });
-  minus.addEventListener("click", function () {
-    let input = document.getElementById("child-age");
-    let val = input.value;
-    if (val > 0) {
-      input.value = Number(val) - 1;
-    }
+  let customMarker = L.marker([48.8584, 2.2945], { icon: customIcon }).addTo(
+    map
+  );
+
+  customMarker.addEventListener("click", () => {
+    console.log("marker click");
+    document.querySelector(".customPopup").classList.add("show");
+  });
+  map.on("move", () => {
+    document.querySelector(".customPopup").classList.remove("show");
+  });
+  map.on("click", () => {
+    document.querySelector(".customPopup").classList.remove("show");
+  });
+  body.addEventListener("click", () => {
+    //document.querySelector(".customPopup").classList.remove("show");
   });
 };
