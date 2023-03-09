5ad4b3faf05b42b596bf7aec50d9fd9e7af1d491
m.liwen <m.liwen@perfectone.pl>
2023-03-09 02:07:07 +0100

    type check

--- public/script.js
+++ public/script.js
@@ -181,44 +181,42 @@ window.onload = (event) => {
       }
     });
   }
+  if (typeof L !== undefined) {
+    var map = L.map("map");
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      // Specify the maximum zoom of the map
+      maxZoom: 19,
 
-  var map = L.map("map");
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    // Specify the maximum zoom of the map
-    maxZoom: 19,
+      // Set the attribution for OpenStreetMaps
+      attribution:
+        '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
 
-    // Set the attribution for OpenStreetMaps
-    attribution:
-      '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map);
+    // Set the view of the map
+    // with the latitude, longitude and the zoom value
+    map.setView([48.8584, 2.2945], 16);
 
-  // Set the view of the map
-  // with the latitude, longitude and the zoom value
-  map.setView([48.8584, 2.2945], 16);
+    var customIcon = L.icon({
+      iconUrl: "/assets/marker.svg",
+      iconSize: [38, 95], // size of the icon
+      //shadowSize: [50, 64], // size of the shadow
+      //iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+      //shadowAnchor: [4, 62], // the same for the shadow
+      //popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+    });
+    let customMarker = L.marker([48.8584, 2.2945], { icon: customIcon }).addTo(
+      map
+    );
 
-  var customIcon = L.icon({
-    iconUrl: "/assets/marker.svg",
-    iconSize: [38, 95], // size of the icon
-    //shadowSize: [50, 64], // size of the shadow
-    //iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-    //shadowAnchor: [4, 62], // the same for the shadow
-    //popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
-  });
-  let customMarker = L.marker([48.8584, 2.2945], { icon: customIcon }).addTo(
-    map
-  );
-
-  customMarker.addEventListener("click", () => {
-    console.log("marker click");
-    document.querySelector(".customPopup").classList.add("show");
-  });
-  map.on("move", () => {
-    document.querySelector(".customPopup").classList.remove("show");
-  });
-  map.on("click", () => {
-    document.querySelector(".customPopup").classList.remove("show");
-  });
-  body.addEventListener("click", () => {
-    //document.querySelector(".customPopup").classList.remove("show");
-  });
+    customMarker.addEventListener("click", () => {
+      console.log("marker click");
+      document.querySelector(".customPopup").classList.add("show");
+    });
+    map.on("move", () => {
+      document.querySelector(".customPopup").classList.remove("show");
+    });
+    map.on("click", () => {
+      document.querySelector(".customPopup").classList.remove("show");
+    });
+  }
 };
